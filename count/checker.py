0129cf590e987e845ba8fe49a349f5f94a707dd6
unknown <narek.saribekyan@gmail.com>
2014-04-06 21:51:32 +0400

    Checker and solution fixed

--- count/checker.py
+++ count/checker.py
@@ -1,29 +1,44 @@
+#!/usr/bin/python
 import sys
+
 
 WA = 1
 OK = 0
+PE = 2
 
 f = [0, 1]
 N = 2000
 for i in xrange(2, N+1):
     f.append(f[i-1] + f[i-2])
 
-inp = open(sys.argv[1], 'r').readline()
-out = open(sys.argv[2], 'r').readline()
+inp = open(sys.argv[1], 'r').readline().rstrip()
+out = open(sys.argv[2], 'r').readline().rstrip()
 if len(out) > 2000:
     exit(WA)
+	
+for i in xrange(0, len(inp) - 1):
+	if inp[i] != '0' and inp[i] != '1':
+		exit(PE)
 
 inp_val = 0
 for i in xrange(0, len(inp) - 1):
-    inp_val += f[len(inp) - i - 2] * int(inp[i])
+    inp_val += f[len(inp) - i - 1] * int(inp[i])
+	
+print "input = " + str(inp_val)
 inp_val += 1
 
 out_val = 0
+
+print "len(out) = " + str(len(out))
 for i in xrange(0, len(out) - 1):
-    out_val += f[len(out) - i - 2] * int(out[i])
+    out_val += f[len(out) - i - 1] * int(out[i])
+	
+print "output = " + str(out_val) 
 
 if inp_val == out_val:
-    exit(OK)
-
+	print "ok"
+	exit(OK)
+	
+print "wa"
 exit(WA)
 
--- count/sol.cpp
+++ count/sol.cpp
@@ -3,63 +3,14 @@
 #include <string>
 using namespace std;
 
-// Normalizes the input. See readme.txt
-void normalize(string &digits) {
-    int n = digits.size();
-    digits = "000" + digits;
-    digits[digits.size() - 1] = '0';
-    if (digits[digits.size() - 2] == '1' && digits[digits.size() - 3] == '0') {
-        digits[digits.size() - 3] = '1';
-        digits[digits.size() - 2] = '0';
-    }
-    for (int iter = 0; iter < digits.size(); ++iter) {
-        for (int i = 2; i < digits.size(); ++i)
-            if (digits[i] == '1' && digits[i - 1] == '1') {
-                digits[i - 2] = '1';
-                digits[i] = digits[i - 1] = '0';
-            }
-    }
-    int p;
-    for (p = 0; p < digits.size() && digits[p] == '0'; ++p);
-    if (p == digits.size())
-        digits = "0";
-    else
-        digits = digits.substr(p);
-}
-long long get_value(string digits) {
-     vector<long long> f;
-     f.push_back(0);
-     f.push_back(1);
-     for (int i = 2; i <= 80; ++i)
-         f.push_back(f[i-1] + f[i-2]);
-     long long ret = 0;
-     for (int i = 0; i < digits.size(); ++i)
-         if (digits[digits.size() - i - 1] == '1')
-             ret += f[i];
-     return ret;
-}
 int main() {
     string digits;
     cin >> digits;
-    //cout << get_value(digits) << '\n';
-    normalize(digits);
-    //cout << get_value(digits) << '\n';
-    cerr << digits << '\n';
-
-    if (digits.size() == 1) {
-        cout << "100\n";
-        return 0;
-    }
-    if (digits.size() == 3) {
-        cout << "1000\n";
-        return 0;
-    }
-    //cerr << digits << '\n';
-    digits[digits.size() - 3] = '1';
-    //cerr << digits << '\n';
-    normalize(digits);
-    //cout << get_value(digits) << '\n';
-    cout << digits << '\n';
+	digits = "0" + digits;	
+	int i;
+	for (i = digits.size() - 2; i >= 0 && digits[i] == '1'; digits[i--]='0');		
+	digits[i] = digits[i + 1] = '1';
+    cout << digits << endl;
     return 0;
 }
 
